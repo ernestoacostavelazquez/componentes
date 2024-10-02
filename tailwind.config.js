b9a7bdf6cfaea2aad8f66ad09d6b42ad44c248fb
ernestoacostavelazquez <ernesto.acostavelazquez@gmail.com>
2024-10-01 22:27:56 -0600

    actualizacion 01/10/2024

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+ // este es para table08 --> darkMode: 'class',
+  theme: {
+    extend: {
+      /* estas lineas es para responsivo02.html pueda funcionar la opción tablet
+      screens: {
+        'tablet': '900px'
+      },
+      */    
+    },
+  },
+  plugins: [],
+}
+
